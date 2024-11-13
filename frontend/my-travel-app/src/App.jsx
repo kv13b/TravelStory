f5f03f71492d4f79bd87fc6d500e9af7627e0cd9
karthik <karthikbhatt22@gmail.com>
2024-11-13 06:22:10 +0530

    added packages

--- frontend/my-travel-app/src/App.jsx
+++ frontend/my-travel-app/src/App.jsx
@@ -1,5 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "./page/Auth/Login";
+import Signup from "./page/Auth/Signup";
+import Home from "./page/Auth/Home/Home";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
