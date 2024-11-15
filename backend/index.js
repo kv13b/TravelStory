e7b975c87b23f4b0c35614e2e275c85f26d1bf02
karthik <karthikbhatt22@gmail.com>
2024-11-15 06:22:47 +0530

    user auth

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,33 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const config = "./config.json";
 const mongoose = require("mongoose");
+const user = require("./models/user.model");
 
 // mongoose.connect(config.connectionString);
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 
-app.get("/hello", async (req, res) => {
-  return res.status(200).json({ message: "hello" });
+app.get("/create-user", async (req, res) => {
+  const { fullname, email, password } = req.body;
+  if (!fullname || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "All feilds are required" });
+  }
+  const isUser = await user.findOne({ email });
+  if (isUser) {
+    return res
+      .status(400)
+      .json({ error: true, message: "User already exists" });
+  }
+  const hashedpassword = await bcrypt.hash(password, 10);
+  const user = new User({
+    fullname,
+    email,
+    password: hashedpassword,
+  });
+  await user.save();
 });
 app.listen(8000);
 module.exports = app;
