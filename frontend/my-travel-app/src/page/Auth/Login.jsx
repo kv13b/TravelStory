1f0b4d0e2118661a8ccf5355ca2baa124cfa8f6a
karthik <karthikbhatt22@gmail.com>
2024-12-12 06:31:23 +0530

    login ui design

--- frontend/my-travel-app/src/page/Auth/Login.jsx
+++ frontend/my-travel-app/src/page/Auth/Login.jsx
@@ -4,25 +4,27 @@ function Login() {
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
         <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
           <div>
-            <h4 className="">
+            <h4 className="text-5xl text-white font-semibold leading-[58px]">
               Capture Your <br /> Journeys
             </h4>
-            <p className="">Recored Your travel experience and memories</p>
+            <p className="text-[15px] text-white leading-6 pr-7 mt-4 ">
+              Recored Your travel experience and memories
+            </p>
           </div>
         </div>
 
-        <div className="">
+        <div className="w-2/4 h-[75vh] bg-white rounded-r-lg relative p-16 shadow-lg shadow-cyan-200">
           <form onSubmit={() => {}}>
             <h4 className="text-2xl font-semibold nb-7">Login</h4>
-            <input type="text " placeholder="Email" className="inout-box" />
+            <input type="text " placeholder="Email" className="input-box" />
             <button type="submit" className="btn-primary">
               LOGIN
             </button>
-            <p className="">OR</p>
+            <p className="text-x5 text-state-500 text-center my-4">OR</p>
 
             <button
               type="submit"
-              className=""
+              className="btn-primary btn-light"
               onClick={() => Navigate("/signup")}
             >
               CREATE ACCOUNT
