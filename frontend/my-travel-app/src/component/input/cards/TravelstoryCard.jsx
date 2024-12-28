81921c096944a11f06467e2a971bf9365b3e0ef9
karthik <karthikbhatt22@gmail.com>
2024-12-28 06:29:02 +0530

    travel card uploads

--- frontend/my-travel-app/src/component/input/cards/TravelstoryCard.jsx
+++ frontend/my-travel-app/src/component/input/cards/TravelstoryCard.jsx
@@ -1,5 +1,6 @@
 import { FaHeart } from "react-icons/fa6";
 import { GrMapLocation } from "react-icons/gr";
+import moment from "moment/moment";
 
 const TravelstoryCard = ({
   imageUrl,
@@ -9,6 +10,7 @@ const TravelstoryCard = ({
   visitedLoaction,
   isFav,
   onFavClick,
+  onClick,
 }) => {
   return (
     <div className="border rounded-lg overflow-hidden bg-white hover:shadow-lg hover:shadow-slate-200 transition-all ease-in-out relative cursor-pointer">
@@ -18,6 +20,17 @@ const TravelstoryCard = ({
         className="w-full object-cover rounded-lg"
         onClick={onclick}
       />
+
+      <div className="p-4" onClick={onclick}>
+        <div className="flex items-center gap-3">
+          <div className="flex-1">
+            <h6 className="flex-sm font-medium">{title}</h6>
+            <span className="text-xs text-slate-500">
+              {date ? moment(date).format("Do MMM YYYY") : "-"}
+            </span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
