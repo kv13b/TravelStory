01d2abd189978ffbb24faf8c76fdb395b8253e67
karthik <karthikbhatt22@gmail.com>
2025-01-05 11:20:52 +0530

    travel card add update

--- frontend/my-travel-app/src/page/Auth/Home/AddEditTravelStories.jsx
+++ frontend/my-travel-app/src/page/Auth/Home/AddEditTravelStories.jsx
@@ -1,10 +1,26 @@
+import { MdAdd } from "react-icons/md";
+
 function AddEditTravelStories({
   type,
   storyInfo,
   setOpenAddEditModel,
   getAllTravelStories,
 }) {
-  return <div>AddEditTravelStories</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Story" : "Update Story"}
+        </h5>
+      </div>
+
+      <div className="">
+        <button className="btn-small" onClick={() => {}}>
+          <MdAdd className="text-lg">ADD STORY</MdAdd>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default AddEditTravelStories;
