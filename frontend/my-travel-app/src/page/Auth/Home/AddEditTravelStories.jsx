7cb74974e879633a17d92de06e47fb587cfe4b99
karthik <karthikbhatt22@gmail.com>
2025-01-06 06:32:10 +0530

    travel story add card

--- frontend/my-travel-app/src/page/Auth/Home/AddEditTravelStories.jsx
+++ frontend/my-travel-app/src/page/Auth/Home/AddEditTravelStories.jsx
@@ -1,23 +1,31 @@
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdClose } from "react-icons/md";
 
 function AddEditTravelStories({
   type,
   storyInfo,
-  setOpenAddEditModel,
+  onClose,
   getAllTravelStories,
 }) {
+  console.log(type);
   return (
     <div>
       <div className="flex items-center justify-between">
         <h5 className="text-xl font-medium text-slate-700">
-          {type === "add" ? "Add Story" : "Update Story"}
+          {type === "Add" ? "Add Story" : "Update Story"}
         </h5>
-      </div>
 
-      <div className="">
-        <button className="btn-small" onClick={() => {}}>
-          <MdAdd className="text-lg">ADD STORY</MdAdd>
-        </button>
+        <div>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+            <button className="btn-small" onClick={() => {}}>
+              <MdAdd className="text-lg" />
+              ADD STORY
+            </button>
+
+            <button className="" onClick={onClose}>
+              <MdClose className="text-xl text-slate-400" />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
