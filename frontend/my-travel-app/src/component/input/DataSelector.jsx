3584242dae1eee56638ab125014a8a40cd519aec
karthik <karthikbhatt22@gmail.com>
2025-01-08 21:11:28 +0530

    date component in travel card edit

--- frontend/my-travel-app/src/component/input/DataSelector.jsx
+++ frontend/my-travel-app/src/component/input/DataSelector.jsx
@@ -2,11 +2,17 @@ import { DayPicker } from "react-day-picker";
 import moment from "moment";
 import { MdOutlineDateRange } from "react-icons/md";
 
-const DataSelector = () => {
+const DataSelector = ({ date, setdate }) => {
   return (
     <div>
-      <button className="" onClick={() => {}}>
+      <button
+        className="inline-flex  items-center gap-2 text-[13px] font-medium text-sky-600 bg-sky-200/40 hover:bg-sky-200/70 rounded-px-2 py-1 cursor-pointer"
+        onClick={() => {}}
+      >
         <MdOutlineDateRange className="text-lg" />
+        {date
+          ? moment(date).format("Do MMM YYYY")
+          : moment().format("Do MMM YYYY")}
       </button>
     </div>
   );
