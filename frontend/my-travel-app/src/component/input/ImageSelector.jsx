fbf2268603a2e78a016b59f6b75e8eac858f7614
karthik <karthikbhatt22@gmail.com>
2025-01-11 06:28:23 +0530

    added image component

--- frontend/my-travel-app/src/component/input/ImageSelector.jsx
+++ frontend/my-travel-app/src/component/input/ImageSelector.jsx
@@ -1,5 +1,34 @@
+import { useRef, useState } from "react";
+import { FaRegFileImage } from "react-icons/fa6";
+
 const ImageSelector = ({ image, setimage }) => {
-  return <div>ImageSelector</div>;
+  const InputRef = useRef(null);
+  const [previewUrl, setPreviewUrl] = useState(null);
+
+  const handleImageChange = () => {};
+  const onchooseFile = () => {
+    InputRef.current.click();
+  };
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/*"
+        ref={InputRef}
+        onChange={handleImageChange}
+        className="hidden"
+      />
+      <button
+        className="w-full h-[220px] flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50 "
+        onClick={() => onchooseFile()}
+      >
+        <div className="w-14 h-14 flex items-center justify-center bg-cyan-50 rounded-full border border-cyan-100">
+          <FaRegFileImage className="text-xl text-cyan-500" />
+        </div>
+        <p className="text-sm text-slate-500">Browse image files to upload</p>
+      </button>
+    </div>
+  );
 };
 
 export default ImageSelector;
