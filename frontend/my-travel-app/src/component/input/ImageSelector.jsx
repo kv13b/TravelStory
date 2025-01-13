290ce0f6194a0064de3da5a9c4c4fbea27dd4f35
karthik <karthikbhatt22@gmail.com>
2025-01-13 06:26:17 +0530

    image in edit component

--- frontend/my-travel-app/src/component/input/ImageSelector.jsx
+++ frontend/my-travel-app/src/component/input/ImageSelector.jsx
@@ -1,9 +1,25 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { FaRegFileImage } from "react-icons/fa6";
 
 const ImageSelector = ({ image, setimage }) => {
   const InputRef = useRef(null);
   const [previewUrl, setPreviewUrl] = useState(null);
+
+  useEffect(() => {
+    if (typeof image === "string") {
+      setPreviewUrl(image);
+    } else if (image) {
+      setPreviewUrl(URL.createObjectURL(image));
+    } else {
+      setPreviewUrl(null);
+    }
+
+    return () => {
+      if (previewUrl && typeof previewUrl === "string" && !image) {
+        URL.revokeObjectURL(previewUrl);
+      }
+    };
+  }, [image]);
 
   const handleImageChange = (event) => {
     const file = event.target.files[0];
@@ -23,15 +39,25 @@ const ImageSelector = ({ image, setimage }) => {
         onChange={handleImageChange}
         className="hidden"
       />
-      <button
-        className="w-full h-[220px] flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50 "
-        onClick={() => onchooseFile()}
-      >
-        <div className="w-14 h-14 flex items-center justify-center bg-cyan-50 rounded-full border border-cyan-100">
-          <FaRegFileImage className="text-xl text-cyan-500" />
+      {!image ? (
+        <button
+          className="w-full h-[220px] flex flex-col items-center justify-center gap-4 bg-slate-50 rounded border border-slate-200/50 "
+          onClick={() => onchooseFile()}
+        >
+          <div className="w-14 h-14 flex items-center justify-center bg-cyan-50 rounded-full border border-cyan-100">
+            <FaRegFileImage className="text-xl text-cyan-500" />
+          </div>
+          <p className="text-sm text-slate-500">Browse image files to upload</p>
+        </button>
+      ) : (
+        <div className="w-full relative">
+          <img
+            src={previewUrl}
+            alt="selected"
+            className="w-full h-[300px] object-cover rounded-lg"
+          />
         </div>
-        <p className="text-sm text-slate-500">Browse image files to upload</p>
-      </button>
+      )}
     </div>
   );
 };
