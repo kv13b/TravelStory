236f7a474dacb1200afdb15dc5339382dea80804
karthik <karthikbhatt22@gmail.com>
2025-01-16 06:28:26 +0530

    location in add component

--- frontend/my-travel-app/src/component/input/TagInput.jsx
+++ frontend/my-travel-app/src/component/input/TagInput.jsx
@@ -1,19 +1,57 @@
 import { useState } from "react";
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdClose } from "react-icons/md";
+import { GrMapLocation } from "react-icons/gr";
 
 const TagInput = ({ tags, setTags }) => {
-  const [inputValue, setinputValue] = useState([]);
-  const addnewTag = () => {};
+  const [inputValue, setinputValue] = useState("");
+  const handleInputChange = (e) => {
+    setinputValue(e.target.value);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addnewTag();
+    }
+  };
+  const addnewTag = () => {
+    if (inputValue.trim() !== "") {
+      setTags([...tags, inputValue.trim()]);
+      setinputValue("");
+    }
+  };
+  const handleRemoveTag = (tagtoRemove) => {
+    setTags(tags.filter((tag) => tag != tagtoRemove));
+  };
   return (
     <div>
+      {tags.length > 0 && (
+        <div className="flex items-center gap-2 flex-wrap mt-2 ">
+          {tags.map((tag, index) => (
+            <span
+              key={index}
+              className="flex items-center gap-2 text-sm text-cyan-600 bg-cyan-200/40 px-3 py-1 rounded"
+            >
+              <GrMapLocation className="text-sm" />
+              {tag}
+              <button onClick={() => handleRemoveTag(tag)}>
+                <MdClose />
+              </button>
+            </span>
+          ))}
+        </div>
+      )}
       <div className="flex items-center gap-4 mt-3">
         <input
           type="text"
           value={inputValue}
           className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
           placeholder="Add Location"
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
         />
-        <button className="" onClick={addnewTag}>
+        <button
+          className="w-8 h-8 flex items-center justify-center rounded border border-cyan-500 hover:bg-cyan-500"
+          onClick={addnewTag}
+        >
           <MdAdd className="text-2xl text-cyan-500 hover:text-white" />
         </button>
       </div>
